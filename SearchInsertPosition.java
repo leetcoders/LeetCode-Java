c4b11fb4c96534f9af807786ab0f7333ef372cfd
applewjg <wangjingui1988@163.com>
2014-12-22 23:41:41 +0800

    SearchInsertPosition
    Change-Id: I158ca57848a89459d37482c694971059917ab25c

---
+++ SearchInsertPosition.java
@@ -0,0 +1,30 @@
+/*
+ Author:     King, wangjingui@outlook.com
+ Date:       Dec 20, 2014
+ Problem:    Search Insert Position
+ Difficulty: Easy
+ Source:     http://leetcode.com/onlinejudge#question_35
+ Notes:
+ Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+ You may assume no duplicates in the array.
+ Here are few examples.
+ [1,3,5,6], 5 -> 2
+ [1,3,5,6], 2 -> 1
+ [1,3,5,6], 7 -> 4
+ [1,3,5,6], 0 -> 0
+
+ Solution: Binary search.
+ */
+
+public class Solution {
+    public int searchInsert(int[] A, int target) {
+        int i = 0, j = A.length - 1;
+        while (i <= j) {
+            int mid = (i + j) / 2;
+            if (A[mid] == target) return mid;
+            if (A[mid] < target) i = mid + 1;
+            else j = mid - 1;
+        }
+        return i;
+    }
+}
