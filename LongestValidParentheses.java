b4f77c23753dc29ff9899dc1b0083c4efe211275
applewjg <wangjingui1988@163.com>
2014-12-22 23:34:20 +0800

    LongestValidParentheses
    Change-Id: I4f04cf277e6b91fb7e3dbb377f824237fb96b26c

---
+++ LongestValidParentheses.java
@@ -0,0 +1,49 @@
+/*
+ Author:     King, wangjingui@outlook.com
+ Date:       Dec 20, 2014
+ Problem:    Longest Valid Parentheses
+ Difficulty: Easy
+ Source:     https://oj.leetcode.com/problems/longest-valid-parentheses/
+ Notes:
+ Given a string containing just the characters '(' and ')', find the length of the longest valid 
+ (well-formed) parentheses substring.
+ For "(()", the longest valid parentheses substring is "()", which has length = 2.
+ Another example is ")()())", where the longest valid parentheses substring is "()()", 
+ which has length = 4.
+
+ Solution: O(n).
+ */
+
+public class Solution {
+    public int longestValidParentheses_1(String s) {
+        Stack<Integer> stk = new Stack<Integer>();
+        int res = 0, count = 0;
+        for(int i = 0; i < s.length(); ++i) {
+            if (s.charAt(i) == '(') {
+                stk.push(count);
+                count = 0;
+            } else if (stk.empty() == false) {
+                count += (1 + stk.pop());
+                res = Math.max(res, count);
+            } else {
+                count = 0;
+            }
+        }
+        return res * 2;
+    }
+    public int longestValidParentheses(String s) {
+        int n = s.length();
+        if (n <= 1) return 0;
+        int res = 0;
+        int[] f = new int[n];
+        for(int i=n-2;i>=0;i--){
+            int match = i + f[i+1] + 1;
+            if(match<n&&s.charAt(i)=='('&&s.charAt(match)==')'){
+                f[i]=f[i+1]+2;
+                if(match+1<n) f[i]+=f[match+1];
+            }
+            res = Math.max(res,f[i]);
+        }
+        return res;
+    }
+}
