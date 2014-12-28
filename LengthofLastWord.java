69b90abd2e44338706e93ec57eefe2c3c99f2875
applewjg <wangjingui1988@163.com>
2014-12-28 18:41:49 +0800

    LengthofLastWord
    Change-Id: I3db3c8b5e2ffbfcb06f1c74d9ff6020362b93041

---
+++ LengthofLastWord.java
@@ -0,0 +1,26 @@
+/*
+ Author:     King, wangjingui@outlook.com
+ Date:       Dec 25, 2014
+ Problem:    Length of Last Word
+ Difficulty: Easy
+ Source:     https://oj.leetcode.com/problems/length-of-last-word/
+ Notes:
+ Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+ If the last word does not exist, return 0.
+ Note: A word is defined as a character sequence consists of non-space characters only.
+ For example, 
+ Given s = "Hello World",
+ return 5.
+
+ Solution: ...
+ */
+public class Solution {
+    public int lengthOfLastWord(String s) {
+        int res = 0, i = s.length() - 1;
+        while (i >= 0 && s.charAt(i) == ' ') --i;
+        while (i >= 0 && s.charAt(i) != ' ') {
+            --i; ++res;
+        }
+        return res;
+    }
+}
