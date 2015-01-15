a506c74e52c31608a2fb4e6f20c80d6f7ab10ac6
applewjg <wangjingui1988@163.com>
2015-01-16 00:32:21 +0800

    add solution
    Change-Id: Ic1bd940f0fbac1a9a73f4057efdb1f15b1adf460

--- SubstringwithConcatenationofAllWords.java
+++ SubstringwithConcatenationofAllWords.java
@@ -1,5 +1,5 @@
 /*
- Author:     King, wangjingui@outlook.com
+ Author:     Andy, nkuwjg@gmail.com
  Date:       Dec 20, 2014
  Problem:    Substring with Concatenation of All Words
  Difficulty: Easy
@@ -14,10 +14,11 @@
  You should return the indices: [0,9].
  (order does not matter).
 
- Solution: ...
+ Solution: 1. Brute + HashMap.
+           2. Sliding Window + HashMap. from Sun Mian.
 */
 public class Solution {
-    public List<Integer> findSubstring(String S, String[] L) {
+    public List<Integer> findSubstring_1(String S, String[] L) {
         List<Integer> res = new ArrayList<Integer>();
         if(L.length==0 || S==null || S.length()==0) return res;
         int M = S.length(), N = L.length;
@@ -45,4 +46,41 @@ public class Solution {
         }
         return res;
     }
+    
+    public List<Integer> findSubstring_2(String S, String[] L) {
+        List<Integer> list = new ArrayList<>();
+        Map<String, Integer> need = new HashMap<>();
+        for(String str: L) {
+            if(need.containsKey(str)) {
+                need.put(str, need.get(str)+1);
+            } else {
+                need.put(str, 1);
+            }
+        }
+        int n = L[0].length(), m = L.length;
+        for (int i = 0; i < n; ++i) {
+            Map<String, Integer> find = new HashMap<>();
+            for (int start = i, end = i, count = 0; end + n <= S.length(); end += n) {
+                String str = S.substring(end, end + n);
+                if (need.get(str) == null) {
+                    start = end + n;
+                    find.clear();
+                    count = 0;
+                    continue;
+                }
+                while (find.get(str) != null && find.get(str) >= need.get(str)) {
+                    String subStart = S.substring(start, start + n);
+                    find.put(subStart, find.get(subStart) - 1);
+                    start += n;
+                    --count;
+                }
+                find.put(str, (find.get(str) == null ? 0 : find.get(str)) + 1);
+                ++count;
+                if (count != m) continue;
+                list.add(start);
+            }
+        }
+        return list;
+    }
+
 }
