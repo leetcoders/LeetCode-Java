4f6bf5e56c33e3dcb418adbf09debd498c3cb85c
applewjg <wangjingui1988@163.com>
2015-01-29 18:45:00 +0800

    add inorder solution
    Change-Id: I845e188989bfd6ba1f59f4ad1bf6992023220b57

--- ConvertSortedArraytoBinarySearchTree.java
+++ ConvertSortedArraytoBinarySearchTree.java
@@ -7,8 +7,10 @@
  Notes:
  Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
 
- Solution: Recursion.
+ Solution: Recursion. 1. preorder
+                      2. inorder
  */
+
 /**
  * Definition for binary tree
  * public class TreeNode {
@@ -19,7 +21,7 @@
  * }
  */
 public class Solution {
-    public TreeNode sortedArrayToBST(int[] num) {
+    public TreeNode sortedArrayToBST_1(int[] num) {
         return sortedArrayToBSTRe(num, 0, num.length - 1);
     }
     public TreeNode sortedArrayToBSTRe(int[] num, int left, int right) {
@@ -29,6 +31,21 @@ public class Solution {
         TreeNode node = new TreeNode(num[mid]);
         node.left = sortedArrayToBSTRe(num, left, mid - 1);
         node.right = sortedArrayToBSTRe(num, mid + 1, right);
+        return node;
+    }
+    public TreeNode sortedArrayToBST(int[] num) {
+        int[] curidx = new int[1];
+        curidx[0] = 0;
+        return sortedArrayToBSTRe1(num, num.length, curidx);
+    }
+    public TreeNode sortedArrayToBSTRe1(int[] num, int len, int[] curidx) {
+        if (len == 0) return null;
+        if (len == 1) return new TreeNode(num[curidx[0]++]);
+        int mid = len / 2;
+        TreeNode left = sortedArrayToBSTRe1(num, mid, curidx);
+        TreeNode node = new TreeNode(num[curidx[0]++]);
+        node.left = left;
+        node.right = sortedArrayToBSTRe1(num, len - 1 - mid, curidx);
         return node;
     }
 }
