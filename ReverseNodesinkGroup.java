40a5e0f68638475f717a5c9f4a856533ba153c55
applewjg <wangjingui1988@163.com>
2015-02-01 18:57:09 +0800

    Reverse Nodes in K group
    Change-Id: I7b5de33afb6d2940ce74557a1c05779253eeda40

---
+++ ReverseNodesinkGroup.java
@@ -0,0 +1,57 @@
+/*
+ Author:     Andy, nkuwjg@gmail.com
+ Date:       Jan 18, 2015
+ Problem:    Reverse Nodes in k-Group
+ Difficulty: Hard
+ Source:     https://oj.leetcode.com/problems/reverse-nodes-in-k-group/
+ Notes:
+ Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
+ If the number of nodes is not a multiple of k then left-out nodes in the end should remain as it is.
+ You may not alter the values in the nodes, only nodes itself may be changed.
+ Only constant memory is allowed.
+ For example,
+ Given this linked list: 1->2->3->4->5
+ For k = 2, you should return: 2->1->4->3->5
+ For k = 3, you should return: 3->2->1->4->5
+
+ Solution: ...
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (k <= 1) return head;
+        int T = GetLength(head) / k;
+        ListNode dummy = new ListNode(0), cur = head, ins = dummy; 
+        dummy.next = head;
+        while ((T--) != 0) {
+            for (int i = 0; i < k - 1; ++i) {
+                ListNode move = cur.next;
+                cur.next = move.next;
+                move.next = ins.next;
+                ins.next = move;
+            }
+            ins = cur;
+            cur = cur.next;
+        }
+        return dummy.next;
+    }
+    public int GetLength(ListNode head) {
+        int length = 0;
+        while (head != null) {
+            head = head.next;
+            length++;
+        }
+        return length;
+    }
+}
